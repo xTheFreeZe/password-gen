e4b5e7df4da71231f1d1ec3419773bdbc5548263
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-06-21 22:41:59 +0200

    JS Update

--- app.js
+++ app.js
@@ -5,26 +5,35 @@ function Passfunc() {
         "hsb/2",
         "nD92n**",
         "?/$2w",
-        "dnB257$67",
-        "dD/%$§hszh&",
-        "nGs(%&$"
+        "dn__B257$67",
+        "dD/%$__&",
+        "nGs(%&_$"
     ]
 
     const secondletters = [
         "hsn/",
         "K&BD(/(/",
-        "+#/d",
+        "+__#/d",
         "DB&/NS/",
         "NSZ/&/"
     ]
 
     const thirdletters = [
+        "hN_h&/",
+        "ÖK8_7(/",
+        "(())",
+        "**#",
+        "DAIUBW",
+        "*Ä*DA&"
+    ]
+
+    const fourthletters = [
         "hNh&/",
         "ÖK87(/",
-        "()",
+        "(())",
         "**#",
-        "HANS",
-        "*Ä*DA&"
+        "DAIUBW",
+        "__(D__"
     ]
 
     let randomfirstletters = firstletters[Math.floor(Math.random() * firstletters.length)]
@@ -35,4 +44,53 @@ function Passfunc() {
 
 
     document.getElementById("password").innerHTML = randomfirstletters + randomsecondletters + randomthirdletters;
+}
+
+function Passhardfunc() {
+
+
+    const firstletters = [
+        "hsb/2",
+        "nD92n**",
+        "?/$2w",
+        "dn__B257$67",
+        "dD/%$__&",
+        "nGs(%&_$"
+    ]
+
+    const secondletters = [
+        "hsn/",
+        "K&BD(/(/",
+        "+__#/d",
+        "DB&/NS/",
+        "NSZ/&/"
+    ]
+
+    const thirdletters = [
+        "hN_h&/",
+        "ÖK8_7(/",
+        "(())",
+        "**#",
+        "DAIUBW",
+        "*Ä*DA&"
+    ]
+
+    const fourthletters = [
+        "hNh&/",
+        "ÖK87(/",
+        "(())",
+        "**#",
+        "DAIUBW",
+        "__(D__"
+    ]
+
+    let randomfirstletters = firstletters[Math.floor(Math.random() * firstletters.length)]
+    let randomsecondletters = secondletters[Math.floor(Math.random() * secondletters.length)]
+    let randomthirdletters = thirdletters[Math.floor(Math.random() * thirdletters.length)]
+    let randomfourthletters = fourthletters[Math.floor(Math.random() * fourthletters.length)]
+
+
+
+
+    document.getElementById("password").innerHTML = randomfirstletters + randomfourthletters + randomsecondletters + randomthirdletters + randomfirstletters + randomfourthletters + randomthirdletters;
 }
